7269c810fcaee0791082daf041693d021f20f2f2
zapalblizh <zapal.blizh@gmail.com>
2022-12-27 04:11:38 -0500

    Starting Final Project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,15 +12,13 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null, 4))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
